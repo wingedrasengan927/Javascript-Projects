4c957f0e276cd0d491cb7f80424684428f9f9b55
wingedrasengan927 <ms.neerajkrishna@gmail.com>
2020-01-16 10:01:24 +0530

    Declare Audio Object everytime a key is pressed

--- drum kit/index.js
+++ drum kit/index.js
@@ -1,14 +1,5 @@
 //Reference: Angela Yu's 'The Complete Web Developer Bootcamp' Course on Udemy
 
-// declare all the audio objects
-const w_sound = new Audio("./assets/sounds/crash.mp3");
-const a_sound = new Audio("./assets/sounds/kick-bass.mp3");
-const s_sound = new Audio("./assets/sounds/snare.mp3");
-const d_sound = new Audio("./assets/sounds/tom-1.mp3");
-const j_sound = new Audio("./assets/sounds/tom-2.mp3");
-const k_sound = new Audio("./assets/sounds/tom-3.mp3");
-const l_sound = new Audio("./assets/sounds/tom-4.mp3");
-
 // add event listeners to all the button elements
 btnList = document.querySelectorAll(".btn");
 btnListLength = btnList.length;
@@ -32,30 +23,37 @@ document.addEventListener("keydown", (event) => {
 function makeSound(key){
     switch (key) {
         case "w":
+            const w_sound = new Audio("./assets/sounds/crash.mp3");
             w_sound.play();
             return true;
             break;
         case "a":
+            const a_sound = new Audio("./assets/sounds/kick-bass.mp3");
             a_sound.play();
             return true;
             break;
         case "s":
+            const s_sound = new Audio("./assets/sounds/snare.mp3");
             s_sound.play();
             return true;
             break;
         case "d":
+            const d_sound = new Audio("./assets/sounds/tom-1.mp3");
             d_sound.play();
             return true;
             break;
         case "j":
+            const j_sound = new Audio("./assets/sounds/tom-2.mp3");
             j_sound.play();
             return true;
             break;
         case "k":
+            const k_sound = new Audio("./assets/sounds/tom-3.mp3");
             k_sound.play();
             return true;
             break;
         case "l":
+            const l_sound = new Audio("./assets/sounds/tom-4.mp3");
             l_sound.play();
             return true;
             break;
